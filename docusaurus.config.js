8d2c837f79b988e0d4a595821c6055c555293c53
nakedstartup <naked.startup@gmail.com>
2023-01-22 19:35:00 +0000

    Naked startup structure

---
+++ docusaurus.config.js
@@ -0,0 +1,97 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Naked Startup Guide',
+  tagline: 'For founders, CEOs and operators who struggle with finding the right and timely advice for their early-stage startup problems.',
+  url: 'https://nakedstartup.guide',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.jpeg',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'nakedstartupguide', // Usually your GitHub org/user name.
+  projectName: 'naked-startup', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Naked Startup',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.jpeg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'fundraising/fundraising',
+            position: 'left',
+            label: 'Fundraising',
+          },
+          {
+            type: 'doc',
+            docId: 'discovery/discovery',
+            position: 'left',
+            label: 'Discovery',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://twitter.com/NkdStartupGuide ',
+            label: 'Twitter',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
